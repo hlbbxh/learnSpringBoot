ae9138c847773eae1ebeeb7fa83ca8a2c85c9a31
胡炎帝 <yandihu@irissz.com>
2019-04-12 18:07:25 +0800

    添加了404时的页面

--- src/main/java/com/bao/learnSpringBoot/Exception/ExpecttionWithController.java
+++ src/main/java/com/bao/learnSpringBoot/Exception/ExpecttionWithController.java
@@ -1,19 +1,26 @@
 package com.bao.learnSpringBoot.Exception;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.http.HttpRequest;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice //首先是这个注解
 public class ExpecttionWithController {
 	public static final String ERROR_VIEW_INDEX = "error";
 	public static final String ERROR_VIEW_CONTENT = "thymeleaf/errer/error";
 	//出现异常应该会被他捕获
+	
+	//这是404的拦截页面
+	@ExceptionHandler(value = NoHandlerFoundException.class)//404页面
+	public Object NoFoudExceptionHandler(HttpServletRequest request,Exception e)throws Exception{
+      ModelAndView mav = new ModelAndView();
+      mav.setViewName(ERROR_VIEW_INDEX);
+      return mav;
+	}
+	
 	@ExceptionHandler(value = Exception.class)//异常
 	public Object defaultErrorHandler(HttpServletRequest request,Exception e)throws Exception{
 	  //e.printStackTrace();  这串代码打印的是长长的公色错误信息  传统的错误信息
@@ -23,4 +30,5 @@ public class ExpecttionWithController {
       mav.setViewName(ERROR_VIEW_CONTENT);
       return mav;
 	}
+
 }
