fb6dee4d48ad4ff888437ce2becb03f3a298289f
yandihu <1394105227@qq.com>
2019-04-14 20:30:35 +0800

    使ajax请求和web请求都能处理全局异常

--- src/main/java/com/bao/learnSpringBoot/Exception/ExpecttionWithController.java
+++ src/main/java/com/bao/learnSpringBoot/Exception/ExpecttionWithController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import com.bao.learnSpringBoot.Util.JsonResult;
+
 @ControllerAdvice //首先是这个注解
 public class ExpecttionWithController {
 	public static final String ERROR_VIEW_INDEX = "error";
@@ -21,14 +23,38 @@ public class ExpecttionWithController {
       return mav;
 	}
 	
+	/**
+	 * 这个方法进行ajax异常的增强  兼容ajax请求异常捕获
+	 * @param request
+	 * @param e
+	 * @return
+	 * @throws Exception
+	 */
 	@ExceptionHandler(value = Exception.class)//异常
 	public Object defaultErrorHandler(HttpServletRequest request,Exception e)throws Exception{
 	  //e.printStackTrace();  这串代码打印的是长长的公色错误信息  传统的错误信息
-      ModelAndView mav = new ModelAndView();
-      mav.addObject("exception", e);
-      mav.addObject("url",request.getRequestURI());
-      mav.setViewName(ERROR_VIEW_CONTENT);
-      return mav;
+	  if (isAjax(request)) {
+		  return JsonResult.errorException(e.getMessage()); //ajax 异常返回
+	  }else {
+		  ModelAndView mav = new ModelAndView();
+	      mav.addObject("exception", e);
+	      mav.addObject("url",request.getRequestURI());
+	      mav.setViewName(ERROR_VIEW_CONTENT);
+	      return mav; //页面跳转返回
+      }
+	}
+	
+	/**
+	 * 
+	 * @Description: 判断是否是ajax请求
+	 * @author 
+	 * @version V1.0
+	 */
+	public static boolean isAjax(HttpServletRequest httpRequest){
+		//通过请求头信息判断是不是ajax请求
+		return  (httpRequest.getHeader("X-Requested-With") != null  
+					&& "XMLHttpRequest"
+						.equals( httpRequest.getHeader("X-Requested-With").toString()) );
 	}
 
 }
