8281b1d08193f1a0caf27a21084a88c07d3ced13
yandihu <1394105227@qq.com>
2019-04-16 22:26:25 +0800

    添加开启 定时任务 异步调用方法的注解

--- src/main/java/com/bao/learnSpringBoot/LearnSpringBootAppl.java
+++ src/main/java/com/bao/learnSpringBoot/LearnSpringBootAppl.java
@@ -3,14 +3,15 @@ package com.bao.learnSpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-import tk.mybatis.spring.annotation.MapperScan;
-
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 //扫描 mybatis mapper 包路径 
-@MapperScan(basePackages = "com.bao.learnSpringBoot.mapper")
+//@MapperScan(basePackages = "com.bao.learnSpringBoot.mapper")
 //自定义扫描包的路径 默认扫描此启动类下面所有的包       org.n3r.idworker   是mybatis分页插件 包含三个包
-@ComponentScan(basePackages={"com.bao.learnSpringBoot","org.n3r.idworker"})
+//@ComponentScan(basePackages={"com.bao.learnSpringBoot","org.n3r.idworker"})
+@EnableScheduling//打开定时任务  打开即可扫描
+@EnableAsync //启用异步调用
 public class LearnSpringBootAppl {
 
 	public static void main(String[] args) {
