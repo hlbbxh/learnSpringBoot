f02ca3f4825000d1eb32d44689801aa0e4e1ce6e
yandihu <1394105227@qq.com>
2019-05-03 19:20:11 +0800

    修改拦截器到MvcConfig中

--- src/main/java/com/bao/learnSpringBoot/Config/FilterConfig.java
+++ src/main/java/com/bao/learnSpringBoot/Config/FilterConfig.java
@@ -8,6 +8,13 @@ import com.bao.learnSpringBoot.Controller.interceptor.OneInterceptor;
 import com.bao.learnSpringBoot.Controller.interceptor.TwoInterceptor;
 
 //表明当前是一个适配器 
+/**
+ * 现在 可以参考 WebMvcConfigurer 的配置  
+ * WebMvcConfigurerAdapter 这个好像已经过时了  
+ * 重新的方法是一样的 addInterceptors 都是这个
+ * @author Gelil
+ *
+ */
 @Configuration 
 public class FilterConfig extends WebMvcConfigurerAdapter{ //继承WebMvcConfigurerAdapter
 	
@@ -19,12 +26,12 @@ public class FilterConfig extends WebMvcConfigurerAdapter{ //继承WebMvcConfigu
 		//registry.addInterceptor(new OneInterceptor()).addPathPatterns("/tow/**"); //这里的one 是在 OneController 中的requestmapper中 
 								//直接 new 自己拦截器                .添加到 路劲中                     拦截所有的one后面的 url  写 * 拦截所有的url
 		
-		registry.addInterceptor(new TwoInterceptor()).addPathPatterns("/one/**").addPathPatterns("/tow/**"); //拦截
+		//registry.addInterceptor(new TwoInterceptor()).addPathPatterns("/one/**").addPathPatterns("/tow/**"); //拦截
 		
 		//需要注册多个拦截器的时候 直接在后面继续add 链式书写  拦截按注册顺序
 /*		registry.addInterceptor(new TwoInterceptor()).addPathPatterns("/tow/**")
 		                                             .addPathPatterns("/one/**");*/
-
+		//不需要拦截的 .excludePathPatterns(patterns); 这个方法  也可以是多个
 		super.addInterceptors(registry);
 	}
 }
