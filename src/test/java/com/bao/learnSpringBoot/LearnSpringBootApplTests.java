6a20ed3bfc20f7844eb1bc980682644243805158
yandihu <1394105227@qq.com>
2019-05-23 21:15:09 +0800

    rabbitmq测试方法

--- src/test/java/com/bao/learnSpringBoot/LearnSpringBootApplTests.java
+++ src/test/java/com/bao/learnSpringBoot/LearnSpringBootApplTests.java
@@ -2,8 +2,12 @@ package com.bao.learnSpringBoot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.bao.learnSpringBoot.Entity.Order;
+import com.bao.learnSpringBoot.Rabbitmq.OrderSender;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,19 @@ public class LearnSpringBootApplTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Autowired
+	private OrderSender ordersender;
+	
+	@Test
+	public void testrabbitmq() throws Exception {
+		Order order = new Order();
+		order.setId(1);
+		order.setName("哇哈哈");
+		order.setMessageid(00001);
+		ordersender.send(order);
+		System.out.println("消息发送完毕");
 	}
 
 }
