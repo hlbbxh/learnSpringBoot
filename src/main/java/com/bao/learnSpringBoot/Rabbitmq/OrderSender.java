5c2197874cb882c4f3faba2f87e8f225070bd870
yandihu <1394105227@qq.com>
2019-07-01 22:13:38 +0800

    修改mq的发送方法，之前是队列配置错误

--- src/main/java/com/bao/learnSpringBoot/Rabbitmq/OrderSender.java
+++ src/main/java/com/bao/learnSpringBoot/Rabbitmq/OrderSender.java
@@ -18,8 +18,8 @@ public class OrderSender {
 		correlationData.setId(order.getMessageid()+"order");
 		rabbitTemplate.convertAndSend(
 				"order-exchange",//交换机名称 
-				"order-test ", //路由规则
-				order,  // message  需要发送的消息  自动序列化
+				"order.2", //路由规则  这里注意是配置的 是* 还是#   现在配置的时候：order.* 现在只能写一个才可以发过去
+				order,// message  需要发送的消息  自动序列化
 				correlationData);//消息的唯一id correlationData 
 	}
 }
