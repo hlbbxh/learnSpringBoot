a0055975135dd836e1609b759605020ca77e10df
carrot <DELL@DESKTOP-9CM0O04>
2019-09-04 19:16:13 +0800

    jf学习笔记

---
+++ src/main/java/com/bao/learnSpringBoot/learnjiufang/comm/CommonVariable.java
@@ -0,0 +1,15 @@
+package com.bao.learnSpringBoot.learnjiufang.comm;
+
+/**
+ * @author DELL 这里面可以定义一些常用的变量 调用的时候直接使用 CommonVariable.属性即可获取到属性值
+ *  2019年9月4日17:29:25
+ */
+public interface CommonVariable {
+	String MONDAY = "星期一";
+	String TUESDAY = "星期二";
+	String WEDNESDAY = "星期三";
+	String THURSDAY = "星期四";
+	String FRIDAY = "星期五";
+	String SATURDAY = "星期六";
+	String SUNDAY = "星期七";
+}
---
+++ src/main/java/com/bao/learnSpringBoot/learnjiufang/comm/Test.java
@@ -0,0 +1,9 @@
+package com.bao.learnSpringBoot.learnjiufang.comm;
+
+public class Test {
+	public static void main(String[] args) {
+		//使用接口获取 接口的属性值 定义公共变量
+		String friday = CommonVariable.FRIDAY;
+		System.out.println(friday);
+	}
+}
